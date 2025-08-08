ea5350b1b4238cab5723f79102f4aa4b2ddfcbef
Refilwe15 <mrefilwe880@gmail.com>
2025-08-08 12:04:29 +0200

    Add Parter 1 variables

--- icebreaker.js
+++ icebreaker.js
@@ -1,9 +1,27 @@
+
+
+
 const favFoods=["apple","pretzels","mac&cheese"];
 let discountCode="";
 let profilePic="";
 const starID = 12345678901234;
-
 console.log("if i had to pick my favourite food i'd pick : "+favFoods);
 console.log("my discount code is :"+discountCode);
 console.log("profile picture :"+profilePic);
 console.log("the ID of the star is :"+starID);
+
+
+let movie_name = "Snow Piercer";
+let sibling_num = "4";
+let isWatched = false;
+let vacation = {
+      location : "Dubai",
+      country : "United Arab Emirates",
+      spot : "Burj Khalifa",
+      activity : "tour"
+}
+
+console.log("My favourite movie is " + movie_name);
+console.log("I have " + sibling_num + "siblings");
+console.log("Have i ever watched inception movie ? " + isWatched);
+console.log("I would love to go to " + vacation.location + " in " + vacation.country + ", to a spot called " + vacation.spot + ", where I want to " + vacation.activity + ".");
