ad906612e2e8774c81cd4f8c020febacddb06f66
itumeleng-itu <matomempaola@gmail.com>
2025-08-08 11:54:19 +0200

    Add Partner 2 variables

---
+++ icebreaker.js
@@ -0,0 +1,9 @@
+const favFoods=["apple","pretzels","mac&cheese"];
+let discountCode="";
+let profilePic="";
+const starID = 12345678901234;
+
+console.log("if i had to pick my favourite food i'd pick : "+favFoods);
+console.log("my discount code is :"+discountCode);
+console.log("profile picture :"+profilePic);
+console.log("the ID of the star is :"+starID);
